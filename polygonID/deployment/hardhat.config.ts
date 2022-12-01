f17fea54e7ea0cbca679fe13cf5172894303036b
tip0x <adamsaddama@hotmail.com>
2022-12-01 05:29:16 +0100

    repo Update

---
+++ polygonID/deployment/hardhat.config.ts
@@ -0,0 +1,40 @@
+// Imports
+// ========================================================
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomicfoundation/hardhat-toolbox";
+import dotenv from 'dotenv';
+
+// Config
+// ========================================================
+dotenv.config();
+
+// Main Configuration Settings
+// ========================================================
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    mumbai: {
+      url: `${process.env.RPC_MUMBAI_URL || ''}`,
+      accounts: process.env.WALLET_PRIVATE_KEY
+        ? [`0x${process.env.WALLET_PRIVATE_KEY}`]
+        : [],
+      allowUnlimitedContractSize: true
+    }
+  },
+  etherscan: {
+    apiKey: process.env.MUMBAI_API_KEY || undefined,
+  },
+};
+
+// Exports
+// ========================================================
+export default config;
